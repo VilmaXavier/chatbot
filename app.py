6f41013eaa35fa5cc92b09d85e9d3f7aaff3727e
VilmaXavier <156011473+VilmaXavier@users.noreply.github.com>
2024-09-24 23:06:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
+
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
+            tf.config.experimental.set_virtual_device_configuration(
+                gpu,
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])  # Set to 4096MB or appropriate value
     except RuntimeError as e:
         print(e)
+
 from flask import Flask, request, jsonify, render_template
 from transformers import pipeline
 from flask_cors import CORS
